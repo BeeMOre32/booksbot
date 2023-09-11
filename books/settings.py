4b3aa2b7a8bc0e7127800ab01d5b29d49768baac
BeeMOre32 <windowssart01@gmail.com>
2023-09-11 15:40:05 +0900

    asd

--- books/settings.py
+++ books/settings.py
@@ -5,5 +5,6 @@ BOT_NAME = 'books'
 SPIDER_MODULES = ['books.spiders']
 NEWSPIDER_MODULE = 'books.spiders'
 
-ROBOTSTXT_OBEY = True
 HTTPCACHE_ENABLED = True
+ROBOTSTXT_OBEY = False
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
